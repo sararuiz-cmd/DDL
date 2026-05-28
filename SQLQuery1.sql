dbf7b6dae7f6acf4882538ba764bddcd182da304
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-28 11:33:56 -0600

    fix: reparar atributos en tablas Usuario y Cargo

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,17 +5,16 @@ Modulo seguridad: Cargo y Usuario
 */
 use master
 go
-if DB_ID(N'UniversidadDB') is not null
-BEGIN
-    ALTER DATABASE UniversidadDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-END
+if exists(select * from sys.databases where name = 'UniversidadDB')
+begin
+	drop database UniversidadDB
+end
+go
+CREATE DATABASE UniversidadDB;
 GO
-drop database  if exists UniversidadDB
-go
-create database UniversidadDB
-go
 use UniversidadDB
 go
+
 --Schema: Es un contenedor logico que sirve para organizar objetos dentro de una base de datos.
 create schema Academico
 go
@@ -53,33 +52,39 @@ create table Academico.Estudiante(
 go
 
 create table Seguridad.Cargo(
-	id int identity(1,1) primary key
-	, nombres nvarchar(60) not null,
-	apellidos nvarchar(60) not null,
-	created_at datetime default getdate(),
-	updated_at datetime null,
-	deleted_at datetime null,
+    id_cargo INT IDENTITY(1,1) PRIMARY KEY,
+    nombre NVARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL,
+    deleted_at DATETIME NULL,
 
-)
-go
-
+    CONSTRAINT uq_cargo_nombre UNIQUE (nombre)
+);
+GO
 create table Seguridad.Usuario(
-	idUsuario int identity(1,1) primary key,
-	idCargo int not null,
-	, nombres nvarchar(60) not null
-	, apellidos nvarchar(60) not null
-	, fechaNac datetime null
-	, pw varbinary(64) not null
-	, email nvarchar(120) null,
-	created_at datetime default getdate(),
-	updated_at datetime null,
-	deleted_at datetime null
-	constraint uq_email unique(email),
-	constraint ck_email check(email like '%_@_%'),
-	constraint fk_usuario_cargo foreign key (idCargo) references Seguridad.Cargo(id)
+    id_usuario INT IDENTITY(1,1) PRIMARY KEY,
+    cif VARCHAR(8) NOT NULL,
+    nombres NVARCHAR(60) NOT NULL,
+    apellidos NVARCHAR(60) NOT NULL,
+    fechaNac DATETIME NULL,
+    pw VARBINARY(64) NOT NULL,
+    email NVARCHAR(120) NOT NULL,
+    id_cargo INT NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL,
+    deleted_at DATETIME NULL,
 
-)
-go
+    CONSTRAINT uq_usuario_cif UNIQUE (cif),
+    CONSTRAINT uq_usuario_email UNIQUE (email),
+    CONSTRAINT ck_usuario_email CHECK (email LIKE '%_@_%._%'),
+
+    CONSTRAINT fk_usuario_cargo
+        FOREIGN KEY (id_cargo)
+        REFERENCES Seguridad.Cargo(id_cargo)
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE
+);
+GO
 
 --Insert
 insert into Academico.Carrera(nombre,precio)
