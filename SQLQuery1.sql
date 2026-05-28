b20283696f09309b674fe4e6f76994356cf6ac1b
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-28 08:05:23 -0600

    feat: agregar schema y validacion inicial

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,25 @@ Crear una base de datos llamada UniversidadDB la cual maneja dos modulos: Academ
 Modulo academico: Carrera y Estudiante
 Modulo seguridad: Cargo y Usuario
 */
+use master
+go
+if exists(select * from sys.databases where name = 'UniversidadDB')
+begin
+	drop database UniversidadDB
+end
+go
+create database UniversidadDB
+go
+--Schema: Es un contenedor logico que sirve para organizar objetos dentro de una base de datos.
+create schema Academico
+go
+create schema Seguridad
+go
+create table Academico.Carrera(
+	id int primary key identity(1,1),
+	nombre nvarchar(100) not null,
+	precio decimal(10,2),
+	created_at datetime default sysdatetime(),
+	updated_at datetime 
+
+
